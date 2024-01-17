558a0005bc84a2f7e653acc23688d63f91e4fc08
Demetrius81 <shizimaniak@gmail.com>
2024-01-17 16:19:27 +0300

    feat: implement task 2

---
+++ 02-write-file/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
+const fileName = '02-write-file.txt';
+const startMsg = 'Hello. Enter text >';
+const endMsg = 'Goodbye.';
+
+stdout.write(startMsg);
+stdin.on('data', (data) => {
+  if (data) {
+    fs.access();
+    fs.writeFile(path.resolve(__dirname, fileName), data, (err) => {
+      if (err) {
+        stdout.write(err + '\n');
+        process.exit(-1);
+      } else {
+        stdout.write('File written successfully\n');
+        process.exit(0);
+      }
+    });
+  }
+});
+process.on('exit', (code) => {
+  if (code === 0) {
+    stdout.write(endMsg + '\n');
+  } else if (code === 128) {
+    stdout.write('Ctrl + C were pressed. Exiting script...');
+  } else {
+    stdout.write(`Semething wrong. The program exited with code ${code}\n`);
+  }
+});
+process.on('SIGINT', () => {
+  process.exit(128);
+});
